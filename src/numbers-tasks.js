cf378f14859dd5035439bc4e9a5070aaaa334266
Aleksei_Ushakov <argarintem@gmail.com>
2023-12-13 14:47:45 +0300

    feat: coplete tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -115,11 +115,11 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  const scalProduct = x1 * x2 + y1 * y2;
+  const scProduct = x1 * x2 + y1 * y2;
   const vectorLength1 = Math.sqrt(x1 * x1 + y1 * y1);
   const vectorLength2 = Math.sqrt(x2 * x2 + y2 * y2);
 
-  return Math.acos(scalProduct / (vectorLength1 * vectorLength2));
+  return Math.acos(scProduct / (vectorLength1 * vectorLength2));
 }
 
 /**
